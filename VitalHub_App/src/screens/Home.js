639df899429af0295a84bbbd2e3a47e9595a2859
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-05-07 09:21:21 -0300

    :sparkles: MedModal e Card sem dados mocados

--- VitalHub_App/src/screens/Home.js
+++ VitalHub_App/src/screens/Home.js
@@ -159,7 +159,6 @@ export const Home = ({ navigation }) => {
                 visible={showModalApp}
                 setShowModalApp={setShowModalApp}
                 navigation={navigation}
-
             />
 
         </Container>
